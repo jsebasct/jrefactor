08288ee71b1d4f7ce55aeb53dee06fa732a457d3
scruz <jsebas.ct@gmail.com>
2021-06-24 22:46:17 -0300

    refactor: move charge to movie

--- VideoStore/src/main/java/com/book/Rental.java
+++ VideoStore/src/main/java/com/book/Rental.java
@@ -16,36 +16,12 @@ public class Rental {
         this.movie = movie;
     }
 
-    public int getDaysRented() {
-        return daysRented;
-    }
-
     public Movie getMovie() {
         return movie;
     }
 
     double getCharge() {
-        double result = 0;
-
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-
-        return result;
+        return movie.getChargeFromMovie(daysRented);
     }
 
     int getFrequentRenterPoints() {
@@ -53,7 +29,7 @@ public class Rental {
 
         // add bonus for a two day new release rental
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                && getDaysRented() > 1) {
+                && daysRented > 1) {
             frequentRenterPoints++;
         }
         return frequentRenterPoints;
