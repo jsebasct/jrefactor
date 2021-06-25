4079980dd5a9092b5fca9a0948ab60d0449d155d
scruz <jsebas.ct@gmail.com>
2021-06-24 22:08:45 -0300

    refactor: idiomatic java 8

--- VideoStore/src/main/java/com/book/Customer.java
+++ VideoStore/src/main/java/com/book/Customer.java
@@ -5,7 +5,6 @@
 package com.book;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -34,32 +33,22 @@ public class Customer {
 
         for (Rental each : rentals) {
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
         }
 
         //add footer lines
-        result += "Amount owned is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getFrequentRenterPoints()) + " frequent renter points";
+        result += "Amount owned is " + getTotalCharge() + "\n";
+        result += "You earned " + getFrequentRenterPoints() + " frequent renter points";
 
         return result;
     }
 
     private double getTotalCharge() {
-        double totalAmount = 0;
-        for (Rental each : rentals) {
-            totalAmount += each.getCharge();
-        }
-        return totalAmount;
+        return rentals.stream().mapToDouble(Rental::getCharge).sum();
     }
 
     private int getFrequentRenterPoints() {
-        int frequentRenterPoints = 0;
-
-        for (Rental each : rentals) {
-            frequentRenterPoints += each.getFrequentRenterPoints();
-        }
-
-        return frequentRenterPoints;
+        return rentals.stream().mapToInt(t -> t.getFrequentRenterPoints()).sum();
     }
 
 }
