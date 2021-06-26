01faebb6fc37d3f6a700e0d8c57c8d48c4202bd5
scruz <jsebas.ct@gmail.com>
2021-06-26 11:40:32 -0300

    refactor: move getCharge from Movie to Price

--- VideoStore/src/main/java/com/book/NewRelasePrice.java
+++ VideoStore/src/main/java/com/book/NewRelasePrice.java
@@ -1,8 +1,14 @@
 package com.book;
 
 public class NewRelasePrice extends Price{
+
     @Override
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
     }
 }
--- VideoStore/src/main/java/com/book/Price.java
+++ VideoStore/src/main/java/com/book/Price.java
@@ -4,27 +4,6 @@ public abstract class Price {
 
     public abstract int getPriceCode();
 
-    double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                double result1 = 2;
-                if (daysRented > 2) {
-                    result1 += (daysRented - 2) * 1.5;
-                }
-                result = result1;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 
 }
